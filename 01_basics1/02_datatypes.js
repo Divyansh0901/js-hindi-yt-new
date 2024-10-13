c7978e16886e7749eb0e09087f3a865d00ae4f9f
Divyansh0901 <106227941+Divyansh0901@users.noreply.github.com>
2024-10-13 15:18:55 +0000

    commit changes in variable and datatype file

---
+++ 01_basics1/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as a newer version
+// alert(3+3) we are using nodejs, not browser
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+let name = "diyansh"
+let age = 42
+let isLoggedIn = false
+console.log (typeof undefined)
+console.log (typeof null)
---
+++ 01_basics1/variables.js
@@ -0,0 +1,11 @@
+const accountId= 13232
+let accountEmail = "divyansh@google.com"
+var accountPassword= "456454"
+accountCity= "chhatarpur"
+// accountId = 2 //not allowed
+// console.log(accountId); 
+accountEmail= "mansi@google.com"
+accountPassword= "423432"
+accountCity="indore"
+let accountState ;
+console.table([accountId,accountCity,accountEmail,accountPassword ,accountState])
